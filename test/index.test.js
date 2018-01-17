0d9fb9bb97c6e5df68d3b48e05f0a627fdd17dfc
Destin Moulton <destin.moulton@gmail.com>
2018-01-17 11:47:17 -0700

    Debugged fetch mocking with node-fetch and fetch-mock.

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,34 @@
+const chai = require("chai");
+const fetchMock = require("fetch-mock");
+const nodeFetch = require("node-fetch");
+
+const ChronicleConsole = require("../index");
+
+const expect = chai.expect;
+
 describe("ChronicleLogger", () => {
-    it("performs console logging", () => {});
+    describe("performs server logging", () => {
+        let _testServer = "http://testserver.com";
+        let _app = "TestApp";
+
+        beforeEach(() => {
+            global.fetch = nodeFetch;
+            fetchMock.post("*", 200);
+            let config = {
+                server: _testServer,
+                app: _app,
+                clientInfo: {}
+            };
+            ChronicleConsole.init(config);
+        });
+
+        afterEach(() => {
+            fetchMock.reset();
+        });
+
+        it("log()", () => {
+            ChronicleConsole.log("Test String");
+            expect(fetchMock.lastUrl()).to.equal(_testServer);
+        });
+    });
 });
