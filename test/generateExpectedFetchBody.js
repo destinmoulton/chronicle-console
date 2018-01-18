f3594cf588153d607de9a56224088075e192c338
Destin Moulton <destin.moulton@gmail.com>
2018-01-18 14:09:25 -0700

    Refactored the generateExpectedBody function into its own file; added a validation function for the parameters to match the Chronicle Console lib.

---
+++ test/generateExpectedFetchBody.js
@@ -0,0 +1,37 @@
+const _ = require("lodash");
+
+function isParamValid(param) {
+    if (param === null || param === undefined) {
+        return false;
+    }
+
+    if (_.isArray(param) || _.isString(param)) {
+        return param.length > 0;
+    } else if (_.isObject(param)) {
+        return !_.isEmpty(param);
+    }
+    return true;
+}
+
+/**
+ * Generate the expected fetch body.
+ *
+ * @param array params
+ */
+function generateExpectedFetchBody(params) {
+    let data = null;
+    if (params.length > 1) {
+        data = [];
+        for (let i = 0; i < params.length; i++) {
+            if (isParamValid(params[i])) {
+                data.push(JSON.parse(JSON.stringify(params[i])));
+            }
+        }
+    } else {
+        data = params[0];
+    }
+
+    return data;
+}
+
+module.exports = generateExpectedFetchBody;
