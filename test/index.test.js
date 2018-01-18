2fb3e3ecfba74e0b4350115d89987e210dd6d751
Destin Moulton <destin.moulton@gmail.com>
2018-01-18 11:50:48 -0700

    Fixed bug with testing the generated data.

--- test/index.test.js
+++ test/index.test.js
@@ -1,34 +1,95 @@
+const fs = require("fs");
+
 const chai = require("chai");
 const fetchMock = require("fetch-mock");
 const nodeFetch = require("node-fetch");
 
 const ChronicleConsole = require("../index");
+const generateRandomParams = require("./generaterandom");
 
 const expect = chai.expect;
 
+// Create the global fetch method
+//global.fetch = nodeFetch;
+
+const SERVER = "http://testserver.com";
+const APP = "TestApp";
+const EXPECTED_HEADERS = { "Content-Type": "text/plain" };
+const EXPECTED_METHOD = "post";
+
+const BASIC_CONSOLE_METHODS = ["error", "info", "log", "table", "warn"];
+
+function expectedBody(params) {
+    let data = null;
+    if (params.length > 1) {
+        data = [];
+        for (let i = 0; i < params.length; i++) {
+            data.push(JSON.parse(JSON.stringify(params[i])));
+        }
+    } else {
+        data = params[0];
+    }
+
+    return data;
+}
+
+// Monitor all POSTs
+fetchMock.post(SERVER, 200);
+
 describe("ChronicleLogger", () => {
-    describe("performs server logging", () => {
-        let _testServer = "http://testserver.com";
-        let _app = "TestApp";
+    describe("Logs to Server", () => {
+        BASIC_CONSOLE_METHODS.forEach(method => {
+            describe("using console method ", () => {
+                let _randomParams = [];
 
-        beforeEach(() => {
-            global.fetch = nodeFetch;
-            fetchMock.post("*", 200);
-            let config = {
-                server: _testServer,
-                app: _app,
-                clientInfo: {}
-            };
-            ChronicleConsole.init(config);
-        });
+                beforeEach(() => {
+                    let config = {
+                        server: SERVER,
+                        app: APP,
+                        clientInfo: {}
+                    };
+                    ChronicleConsole.init(config);
 
-        afterEach(() => {
-            fetchMock.reset();
-        });
+                    // Generate a new set of random parameters
+                    _randomParams = generateRandomParams();
 
-        it("log()", () => {
-            ChronicleConsole.log("Test String");
-            expect(fetchMock.lastUrl()).to.equal(_testServer);
+                    // Log to file
+                    var toLog =
+                        "\n\nconsole." +
+                        method +
+                        "() ------------ \n" +
+                        JSON.stringify(_randomParams);
+
+                    fs.appendFileSync("./paramlog.txt", toLog);
+                });
+
+                afterEach(() => {
+                    const fetchMockComplete = fetchMock.done();
+                    expect(fetchMockComplete).to.equal(true);
+                    if (!fetchMockComplete) {
+                        console.error(_randomParams);
+                    }
+
+                    expect(fetchMock.lastUrl()).to.equal(SERVER);
+
+                    const request = fetchMock.lastOptions();
+                    expect(request.method).to.equal(EXPECTED_METHOD);
+                    expect(request.headers).to.deep.equal(EXPECTED_HEADERS);
+
+                    const body = JSON.parse(request.body);
+                    expect(body.app).to.equal(APP);
+                    expect(body.type).to.equal(method);
+                    expect(body.info).to.deep.equal(
+                        expectedBody(_randomParams)
+                    );
+
+                    fetchMock.reset();
+                });
+
+                it("." + method + "()", () => {
+                    ChronicleConsole[method].apply(this, _randomParams);
+                });
+            });
         });
     });
 });
