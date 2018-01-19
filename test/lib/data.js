f23102801ccdfe1b7eb6f5cd9c5a8c87d205ffe1
Destin Moulton <destin.moulton@gmail.com>
2018-01-19 11:23:13 -0700

    Added single parameter data.

--- test/lib/data.js
+++ test/lib/data.js
@@ -2,16 +2,62 @@ const generateRandomParams = require("./generaterandom");
 
 module.exports = [
     {
-        name: "Pre-defined Data.",
+        name: "Empty data [Nothing sent to server, console IS logged]",
+        params: ["", {}, [], undefined, null],
+        shouldLogToServer: false
+    },
+    {
+        name: "Single Number",
+        params: [987654321],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single String",
+        params: ["AllGoodDogs with Spaces"],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Array of Numbers",
+        params: [[1, 2, 3]],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Array of Strings",
+        params: [["one", "two", "three"]],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Array of Single Object",
+        params: [[{ one: 1, two: 2, three: 3 }]],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Array of Multiple Objects",
+        params: [[{ one: 1, two: 2, three: 3 }, { alpha: "a", beta: "b" }]],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Object",
+        params: [{ one: 1, two: 2, three: 3 }],
+        shouldLogToServer: true
+    },
+    {
+        name: "Single Object with Array Property",
+        params: [{ alpha: "a", beta: "b", testArray: [1, 2, 3] }],
+        shouldLogToServer: true
+    },
+    {
+        name: "Pre-defined Params",
         params: [
             9876,
             ["one", "two", "three"],
             "A string.",
             { alpha: [0, 1, 2], beta: "Beta string.", theta: 1234 }
-        ]
+        ],
+        shouldLogToServer: true
     },
     {
-        name: "Pre-defined with Empty Data intermixed.",
+        name: "Pre-defined with Empty Params",
         params: [
             9876,
             null,
@@ -22,26 +68,32 @@ module.exports = [
             { alpha: [0, 1, 2], beta: "Beta string.", theta: 1234 },
             "",
             undefined
-        ]
+        ],
+        shouldLogToServer: true
     },
     {
         name: "Random Data 1",
-        params: generateRandomParams()
+        params: generateRandomParams(),
+        shouldLogToServer: true
     },
     {
         name: "Random Data 2",
-        params: generateRandomParams()
+        params: generateRandomParams(),
+        shouldLogToServer: true
     },
     {
         name: "Random Data 3",
-        params: generateRandomParams()
+        params: generateRandomParams(),
+        shouldLogToServer: true
     },
     {
         name: "Random Data 4",
-        params: generateRandomParams()
+        params: generateRandomParams(),
+        shouldLogToServer: true
     },
     {
         name: "Random Data 5",
-        params: generateRandomParams()
+        params: generateRandomParams(),
+        shouldLogToServer: true
     }
 ];
