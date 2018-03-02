109f8a9d5618653ad4c6feae4fd21bb2b8c1cb79
Destin Moulton <destin.moulton@gmail.com>
2018-03-02 11:47:52 -0700

    Many changes adding types and whatnot.

---
+++ src/ChronicleConsole.ts
@@ -0,0 +1,365 @@
+/**
+ * Console methods.
+ */
+
+import GroupStack from "./GroupStack";
+import Helpers from "./Helpers";
+import * as Types from "./types";
+
+export class ChronicleConsole {
+    private _settings: Types.ISettings = {
+        serverURL: "",
+        appName: "",
+        env: null,
+        alsoConsole: true,
+        globalize: true,
+        methodsToLog: ["action", "error", "warn"],
+        customMethods: []
+    };
+
+    private _console = console;
+    private _fetch: GlobalFetch;
+
+    constructor(config) {
+        this.init(config);
+    }
+
+    init(config) {
+        this._settings.serverURL = config.server || "";
+        this._settings.appName = config.app || "";
+        this._settings.env = config.env || null; // The users environment info
+        this._settings.alsoConsole = config.toConsole || true; // Log to the console?
+        this._settings.globalize = config.globalize || true; // Overwrite the global/window console
+
+        // The methods that should be logged to the server
+        this._settings.methodsToLog = config.methodsToLog || [
+            "action",
+            "error",
+            "warn"
+        ];
+
+        // Custom logging methods
+        this._settings.customMethods = config.customMethods || [];
+        _registerCustomMethods();
+
+        // Setup the global console
+        this._console = config.consoleObject || console;
+
+        var windowIsAvailable =
+            typeof window !== "undefined" &&
+            typeof window.navigator === "object";
+
+        if (!this._settings.env && windowIsAvailable) {
+            // Set the default to window navigator if available
+            this._settings.env = window.navigator;
+        }
+
+        if (fetch !== undefined) {
+            // Define the local fetch method
+            this._fetch = fetch;
+        } else {
+            this._console.error(
+                "ChronicleConsole :: No fetch() method defined."
+            );
+        }
+
+        if (this._settings.globalize) {
+            _overwriteGlobalConsole();
+        }
+    }
+
+    private _logIt(data, type) {
+        if (Helpers.isArray(data) || data.length === 0) {
+            // Only allow arrays
+            return true;
+        }
+
+        if (data.length === 1) {
+            // Don't log an array if there is only one
+            // piece of data
+            data = data[0];
+        }
+
+        if (GroupStack.isEmpty) {
+            return this._sendData(data, type);
+        }
+
+        // The zeroth element (head) holds the current group
+        GroupStack.pushLog({ log: data, type });
+    }
+
+    private _sendData(data, type) {
+        if (
+            !this._settings.serverURL ||
+            !this._settings.appName ||
+            !this._settings.env
+        ) {
+            this._console.error(
+                "ChronicleConsole :: No server, app, or client info provided. Run init() first."
+            );
+            return false;
+        }
+
+        var envInfo = {};
+        if (this._settings.env.userAgent) {
+            envInfo = _collateEnvironmentInfo(this._settings.env);
+        }
+
+        var trace = this._stackTrace(4);
+
+        var dataToPost = {
+            app: this._settings.appName,
+            client: envInfo,
+            type: type,
+            data: data,
+            trace: trace
+        };
+
+        var params = {
+            method: "post",
+            body: JSON.stringify(dataToPost),
+            headers: {
+                "Content-Type": "text/plain"
+            }
+        };
+
+        this._fetch(this._settings.serverURL, params);
+
+        return true;
+    }
+
+    private _shouldLog(methodName: string) {
+        if (this._settings.methodsToLog === "all") return true;
+
+        return this._settings.methodsToLog.indexOf(methodName) > -1
+            ? true
+            : false;
+    }
+
+    private _now() {
+        if (
+            typeof performance !== "undefined" &&
+            performance.now !== undefined
+        ) {
+            return performance.now();
+        } else {
+            return Date.now();
+        }
+    }
+
+    // From https://stackoverflow.com/a/635852/470577
+    private _stackTrace(depth) {
+        var stackString = new Error().stack;
+        // Remove the trace lines for this and trace() call
+        // from depth to -1 (the split adds an extra empty element)
+        var trace = stackString.split("\n").slice(depth, -1);
+        return trace;
+    }
+
+    /**
+     * This is a method not provided by the
+     * standard/global console. This method is
+     * used for logging user actions.
+     */
+    action() {
+        if (!this._shouldLog("action")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "action");
+        }
+    }
+
+    assert(assertion) {
+        if (this._settings.alsoConsole)
+            this._console.assert.apply(this, arguments);
+
+        if (!this._shouldLog("assert")) return true;
+
+        if (!assertion) {
+            var args = Helpers.argumentsToArray(arguments);
+            return this._logIt(
+                Helpers.collateArguments(args.slice(1)),
+                "assert"
+            );
+        }
+    }
+
+    error() {
+        if (this._settings.alsoConsole)
+            this._console.error.apply(this, arguments);
+
+        if (!this._shouldLog("error")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "error");
+        }
+    }
+
+    group() {
+        if (this._settings.alsoConsole)
+            this._console.group.apply(this, arguments);
+
+        if (!this._shouldLog("group")) return true;
+
+        GroupStack.addGroup();
+    }
+
+    groupCollapsed() {
+        if (this._settings.alsoConsole)
+            this._console.groupCollapsed.apply(this, arguments);
+
+        if (!this._shouldLog("groupCollapsed")) return true;
+
+        GroupStack.addGroup();
+    }
+
+    groupEnd() {
+        if (this._settings.alsoConsole)
+            this._console.groupEnd.apply(this, arguments);
+
+        if (!this._shouldLog("group") && !this._shouldLog("groupCollapsed"))
+            return true;
+
+        var head = GroupStack.removeGroup();
+        if (!GroupStack.isEmpty()) {
+            // Put the finished group into the still active group
+            GroupStack.pushLog({ type: "group", log: head });
+        } else {
+            return this._logIt([{ type: "group", log: head }], "group");
+        }
+    }
+
+    info() {
+        if (this._settings.alsoConsole)
+            this._console.info.apply(this, arguments);
+
+        if (!this._shouldLog("info")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "info");
+        }
+    }
+
+    log() {
+        if (this._settings.alsoConsole)
+            this._console.log.apply(this, arguments);
+
+        if (!this._shouldLog("log")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "log");
+        }
+    }
+
+    table() {
+        if (this._settings.alsoConsole)
+            this._console.table.apply(this, arguments);
+
+        if (!this._shouldLog("table")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "table");
+        }
+    }
+
+    time(label) {
+        if (this._settings.alsoConsole) this._console.time(label);
+
+        if (!this._shouldLog("time")) return true;
+
+        var timerLabel = label === undefined ? "default" : `${label}`;
+        _timers[timerLabel] = this._now();
+    }
+
+    timeEnd(label) {
+        if (this._settings.alsoConsole) this._console.timeEnd(label);
+
+        if (!this._shouldLog("time")) return true;
+
+        var timerLabel = label === undefined ? "default" : `${label}`;
+
+        if (_timers[timerLabel] !== undefined) {
+            var elapsed = (this._now() - _timers[timerLabel]).toFixed(2);
+            var data = [`${timerLabel}: ${elapsed}ms`];
+            delete _timers[timerLabel];
+            return this._logIt(data, "time");
+        }
+    }
+
+    trace() {
+        if (this._settings.alsoConsole)
+            this._console.trace.apply(this, arguments);
+
+        if (!this._shouldLog("trace")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        var data = [];
+        if (args[0]) {
+            data = Helpers.collateArguments(args);
+        }
+
+        data.push(this._stackTrace(2));
+        return this._logIt(data, "trace");
+    }
+
+    warn() {
+        if (this._settings.alsoConsole)
+            this._console.warn.apply(this, arguments);
+
+        if (!this._shouldLog("warn")) return true;
+
+        var args = Helpers.argumentsToArray(arguments);
+        if (args[0]) {
+            var data = Helpers.collateArguments(args);
+            return this._logIt(data, "warn");
+        }
+    }
+
+    /**
+     * Stubs for console functions that aren't logged
+     */
+    clear() {
+        if (this._settings.alsoConsole) this._console.clear();
+    }
+
+    count() {
+        if (this._settings.alsoConsole)
+            this._console.count.apply(this, arguments);
+    }
+
+    /**
+     * Stubs for Non-Standard console functions
+     */
+    dir() {
+        if (this._settings.alsoConsole && this._console.dir !== undefined)
+            this._console.dir.apply(this, arguments);
+    }
+
+    dirxml() {
+        if (this._settings.alsoConsole && this._console.dirxml !== undefined)
+            this._console.dirxml.apply(this, arguments);
+    }
+
+    profile() {
+        if (this._settings.alsoConsole && this._console.profile !== undefined)
+            this._console.profile.apply(this, arguments);
+    }
+
+    profileEnd() {
+        if (
+            this._settings.alsoConsole &&
+            this._console.profileEnd !== undefined
+        )
+            this._console.profileEnd.apply(this, arguments);
+    }
+}
