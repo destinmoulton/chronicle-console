c2c162d4f075446c8e8b132727d1f1a017faa5ab
Destin Moulton <destin.moulton@gmail.com>
2018-03-05 09:29:34 -0700

    Added webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    entry: "./src/ChronicleConsole.ts",
+    output: {
+        path: path.join(__dirname, "dist/"),
+        filename: "chronicleconsole.js",
+        libraryTarget: "umd"
+    },
+    devtool: "source-map",
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    }
+};
