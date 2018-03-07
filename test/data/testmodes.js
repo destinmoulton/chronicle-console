f882db7d732fcd4f6fc1fb57fbcac746e90565f2
Destin Moulton <destin.moulton@gmail.com>
2018-03-07 14:57:13 -0700

    Created a testmodes file to contain the test modes.

---
+++ test/data/testmodes.js
@@ -0,0 +1,6 @@
+module.exports = [
+    { title: "console OFF, log OFF", consoleEnabled: false, logEnabled: false },
+    { title: "console OFF, log ON", consoleEnabled: false, logEnabled: true },
+    { title: "console ON, log OFF", consoleEnabled: true, logEnabled: false },
+    { title: "console ON, log ON", consoleEnabled: true, logEnabled: true }
+];
